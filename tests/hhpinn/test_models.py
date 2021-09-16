cc60f54f415925c1bdf193e5872f0f48fd11625c
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2021-09-16 13:09:40 +0200

    TST: Add unit tests for save/load of models

--- tests/hhpinn/test_models.py
+++ tests/hhpinn/test_models.py
@@ -35,3 +35,23 @@ class TestHodgeHelmholtzPINN:
 
         for i in range(len(uy)):
             npt.assert_allclose(uy[i], -weights[0])
+
+    def test_save_load_before_training(self, tmpdir):
+        # Variable `tmpdir` is a `pytest` fixture.
+        m1 = HodgeHelmholtzPINN()
+
+        m1.save(tmpdir)
+        m2 = HodgeHelmholtzPINN.load(tmpdir)
+
+        npt.assert_equal(m1.hidden_layers, m2.hidden_layers)
+
+    def test_save_load_after_training(self, tmpdir):
+        m1 = HodgeHelmholtzPINN(hidden_layers=[10], epochs=5, learning_rate=0.1)
+        m1.fit(np.random.normal(size=(10, 2)), np.random.normal(size=(10, 2)))
+
+        m1.save(tmpdir)
+        m2 = HodgeHelmholtzPINN.load(tmpdir)
+
+        for i in range(len(m1.model.trainable_variables)):
+            npt.assert_array_equal(m1.model.trainable_variables[i],
+                             m2.model.trainable_variables[i])
