2be31934cf57b8c918e7412d6aa99ea0103e8001
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2021-10-07 16:28:05 +0200

    Use raw string in docstring

--- src/hhpinn/scoring.py
+++ src/hhpinn/scoring.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def rel_pw_error(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
-    """Return normalized pointwise error: e(x) = || y(x) - \hat y(x) ||."""
+    r"""Return normalized pointwise error: e(x) = || y(x) - \hat y(x) ||."""
     err = np.linalg.norm(y_true - y_pred, 2, axis=1)
     rel_err = err / np.mean(np.linalg.norm(y_true, 2, axis=1))
 
