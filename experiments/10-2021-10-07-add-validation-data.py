901658f1b8897af35d9742752241d80ede0a154a
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2021-10-11 16:26:06 +0200

    Update script for Exp. 10: Plot error fields starting with zero

--- experiments/10-2021-10-07-add-validation-data.py
+++ experiments/10-2021-10-07-add-validation-data.py
@@ -304,21 +304,24 @@ err_u_last = rel_pw_error(test_u, pred_u_last)
 err_u_best = rel_pw_error(test_u, pred_u_best)
 err_max = np.max((err_u_first, err_u_last, err_u_best))
 
-hhpinn.plotting.plot_error_field_2D(test_x, err_u_first, GRID_SIZE, train_x, vmax=err_max)
+hhpinn.plotting.plot_error_field_2D(test_x, err_u_first, GRID_SIZE, train_x,
+                                    vmin=0.0, vmax=err_max)
 
 render_figure(
     to_file=os.path.join("_assets", "error-field-model=first.pdf"),
     save=args["save"]
 )
 
-hhpinn.plotting.plot_error_field_2D(test_x, err_u_last, GRID_SIZE, train_x, vmax=err_max)
+hhpinn.plotting.plot_error_field_2D(test_x, err_u_last, GRID_SIZE, train_x,
+                                    vmin=0.0, vmax=err_max)
 
 render_figure(
     to_file=os.path.join("_assets", "error-field-model=last.pdf"),
     save=args["save"]
 )
 
-hhpinn.plotting.plot_error_field_2D(test_x, err_u_best, GRID_SIZE, train_x, vmax=err_max)
+hhpinn.plotting.plot_error_field_2D(test_x, err_u_best, GRID_SIZE, train_x,
+                                    vmin=0.0, vmax=err_max)
 
 render_figure(
     to_file=os.path.join("_assets", "error-field-model=best.pdf"),
