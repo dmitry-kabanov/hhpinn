941087b5e2b39ccd7be0e5462e4ba2fabb312b0c
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2021-10-12 18:36:38 +0200

    Add function to compute relative MSE

--- src/hhpinn/scoring.py
+++ src/hhpinn/scoring.py
@@ -19,6 +19,18 @@ def mse(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
     return error_mse
 
 
+def rel_mse(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+    r"""Return relative mean squared error."""
+    if y_true.shape != y_pred.shape:
+        logger.warning("`y_true` and `y_pred` have different shapes")
+
+    errors = np.linalg.norm((y_true - y_pred) / y_pred, 2, axis=1)
+    assert len(errors) == len(y_true)
+    error_mse = np.mean(errors)
+
+    return error_mse
+
+
 def rel_pw_error(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
     r"""Return normalized pointwise error: e(x) = || y(x) - \hat y(x) ||."""
     err = np.linalg.norm(y_true - y_pred, 2, axis=1)
