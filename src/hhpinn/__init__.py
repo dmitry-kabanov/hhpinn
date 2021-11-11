57b506265bd8098fe6e9c508f88320df846c0b86
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2021-11-11 18:23:35 +0100

    Add null logger

--- src/hhpinn/__init__.py
+++ src/hhpinn/__init__.py
@@ -1,6 +1,12 @@
 # flake8: noqa
 __version__ = "0.2"
 
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+from logging import NullHandler
+
+logging.getLogger(__name__).addHandler(NullHandler())
+
 from . import datasets
 from . import models
 from . import plotting
