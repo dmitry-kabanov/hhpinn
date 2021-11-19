ae81fbfc2d86f857948d1b59dd550eb9f67e0831
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2021-11-19 12:55:06 +0100

    Rename x_colloc to pts to avoid confusion with variables x and y

--- src/hhpinn/_sobolev3reg.py
+++ src/hhpinn/_sobolev3reg.py
@@ -6,37 +6,37 @@ import tensorflow as tf
 
 
 def sobolev3reg(model):
-    def compute(x_colloc):
+    def compute(pts):
         with tf.GradientTape(
             persistent=True, watch_accessed_variables=False
         ) as t3:
-            t3.watch(x_colloc)
+            t3.watch(pts)
             with tf.GradientTape(
                 persistent=True, watch_accessed_variables=False
             ) as t2:
-                t2.watch(x_colloc)
+                t2.watch(pts)
                 with tf.GradientTape(
                     persistent=True, watch_accessed_variables=False
                 ) as t1:
-                    t1.watch(x_colloc)
-                    f = model(x_colloc)
+                    t1.watch(pts)
+                    f = model(pts)
 
                 # Compute velocity predictions from the stream function `psi`.
                 # To simplify notation, we denote components of the gradient
                 # of f to be (u, v).
-                df = t1.gradient(f, x_colloc)
+                df = t1.gradient(f, pts)
                 u, v = tf.split(df, 2, axis=1)
 
-            grad_u = t2.gradient(u, x_colloc)
-            grad_v = t2.gradient(v, x_colloc)
+            grad_u = t2.gradient(u, pts)
+            grad_v = t2.gradient(v, pts)
 
             du_dx, du_dy = tf.split(grad_u, 2, axis=1)
             dv_dx, dv_dy = tf.split(grad_v, 2, axis=1)
 
-        grad_du_dx = t3.gradient(du_dx, x_colloc)
-        grad_du_dy = t3.gradient(du_dy, x_colloc)
-        grad_dv_dx = t3.gradient(dv_dx, x_colloc)
-        grad_dv_dy = t3.gradient(dv_dy, x_colloc)
+        grad_du_dx = t3.gradient(du_dx, pts)
+        grad_du_dy = t3.gradient(du_dy, pts)
+        grad_dv_dx = t3.gradient(dv_dx, pts)
+        grad_dv_dy = t3.gradient(dv_dy, pts)
 
         d2u_dxx, d2u_dxy = tf.split(grad_du_dx, 2, axis=1)
         d2u_dyx, d2u_dyy = tf.split(grad_du_dy, 2, axis=1)
