4427304f39206ef855d3968fc513d65fdc6780f0
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2022-01-17 14:00:30 +0100

    Make function `rel_pw_error` consistent with `rel_root_mse`

--- src/hhpinn/scoring.py
+++ src/hhpinn/scoring.py
@@ -37,8 +37,21 @@ def rel_root_mse(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
 
 
 def rel_pw_error(y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
-    r"""Return normalized pointwise error: e(x) = || y(x) - \hat y(x) ||."""
-    err = np.linalg.norm(y_true - y_pred, 2, axis=1)
-    rel_err = err / np.mean(np.linalg.norm(y_true, 2, axis=1))
+    r"""Return normalized pointwise error.
+
+    The error is computed as
+    $$
+    e(x_i) = \sqrt{
+        || y(x_i) - \hat y(x_i) ||^2 / (\sum_j || y(x_j) ||^2),
+    }
+    $$
+    that is, in terms of discretized L2 norms.
+
+    This function should give results consistent with `rel_root_mse`.
+    """
+    err = np.linalg.norm(y_true - y_pred, 2, axis=1)**2
+    rel_err = np.sqrt(
+        err / np.sum(np.linalg.norm(y_true, 2, axis=1)**2)
+    )
 
     return rel_err
