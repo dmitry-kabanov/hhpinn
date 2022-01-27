b805e6a6321569adf04e98d79d2faaca909e8260
Dmitry Kabanov <kabanov.dmitry@gmail.com>
2022-01-27 12:50:24 +0100

    TST: Add test for sobolev3reg function with nonconstant derivatives

--- tests/hhpinn/test_sobolev3reg.py
+++ tests/hhpinn/test_sobolev3reg.py
@@ -7,7 +7,7 @@ from hhpinn._sobolev3reg import sobolev3reg
 
 
 class TestSobolev3Reg:
-    def test__linear_function__should_have_zero_third_derivatives(self):
+    def test__simple_case__should_have_expected_constant_values(self):
         x = tf.Variable([
             [1.0, 27.5],
             [2.3, 1.35],
@@ -26,3 +26,37 @@ class TestSobolev3Reg:
         )
 
         npt.assert_allclose(s3_fn, desired, rtol=1e-7, atol=1e-7)
+
+    def test__nonlinear_function__should_have_expected_3rd_derivatives(self):
+        data = tf.random.uniform((20, 2))
+        x, y = tf.split(data, 2, axis=1)
+
+        def u(data):
+            x, y = tf.split(data, 2, axis=1)
+            uu = tf.sin(x + 4*y) + (5*x + 2*y)**4
+            return uu
+
+        s3reg_fn = sobolev3reg(u)
+
+        s3reg = s3reg_fn(data)
+
+        # Results are computed using Mathematica.
+        # dxxx, dxxy, dxyy, dxyx, dyyy, dyyx, dyxx, dyxy.
+        # The derivatives sum of squares is constant in this case.
+        derivs = (
+            3000 * (5 * x + 2 * y) - 1 * tf.cos(x + 4 * y),
+            1200 * (5 * x + 2 * y) - 4 * tf.cos(x + 4 * y),
+            480 * (5 * x + 2 * y) - 16 * tf.cos(x + 4 * y),
+            1200 * (5 * x + 2 * y) - 4 * tf.cos(x + 4 * y),
+            192 * (5 * x + 2 * y) - 64 * tf.cos(x + 4 * y),
+            480 * (5 * x + 2 * y) - 16 * tf.cos(x + 4 * y),
+            1200 * (5 * x + 2 * y) - 4 * tf.cos(x + 4 * y),
+            480 * (5 * x + 2 * y) - 16 * tf.cos(x + 4 * y),
+        )
+
+        sum_of_sq = 0.0
+
+        for der in derivs:
+            sum_of_sq += der**2
+
+        npt.assert_allclose(s3reg, sum_of_sq, rtol=1e-6, atol=1e-7)
